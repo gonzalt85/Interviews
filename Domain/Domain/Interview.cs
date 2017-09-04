39edf74a7f705be9787340c430208f8846a10480
Juan <juanc.gonzalez8@gmail.com>
2017-09-04 14:39:37 -0400

    Interview class

---
+++ Domain/Domain/Interview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Interviews
+{
+    public class Interview
+    {
+        public int Id { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public DateTime TakenOn { get; set; }
+        public string Interviewer { get; set; }
+        public int Job { get; set; }
+        public int InterviewType { get; set; }
+    }
+}
