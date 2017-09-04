0b8bc1a420816dcbe4ce2c4ccc52e4bc2ec575c5
Juan <juanc.gonzalez8@gmail.com>
2017-09-04 14:03:29 -0400

    Domain commit.

---
+++ Domain/Domain/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Job
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Entered { get; set; }
+        public string Duration { get; set; }
+        public string Status { get; set; }
+        public string Requirements { get; set; }
+        public string Notes { get; set; }
+        public string Location { get; set; }
+        public string Contacts { get; set; }
+    }
+}
